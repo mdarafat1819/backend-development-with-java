f934873a6a9b071842990e0300954e0bd4dbc138
Arafat <mdarafat1819@gmail.com>
2026-02-02 00:16:33 +0600

    Configure PostgreSQL and Swagger

---
+++ src/main/java/com/example/stms_multi_user/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.stms_multi_user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Well come to Stms_Multi_User Apps";
+    }
+}
